deaf690d9cf91bd69b3f1eee4af04c24704365ba
DavidHdw <tossboydavid@sina.com>
2014-10-30 17:48:04 +0800

    refactoring for change: send json rather than render template on server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
-  def broadcast(channel, &blk)
-    message = {channel:channel, data:capture(&blk), ext:{auth_token:FAYE_TOKEN}}
+  def broadcast_data(channel, data)
+    message = {channel:channel, data:data, ext:{auth_token:FAYE_TOKEN}}
     uri = URI.parse('http://localhost:9292/faye')
     Net::HTTP.post_form(uri, message:message.to_json)
+  end
+
+  def broadcast(channel, &blk)
+    broadcast_data(channel,capture(&blk))
+  end
+
+  def broadcast_json(channel, object)
+    broadcast_data(channel, object.to_json)
   end
 end
